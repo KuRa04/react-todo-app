7b580ab77decdd5ddac74eb9e8faf6dfd5226682
Daichi <50697314+KuRa04@users.noreply.github.com>
2021-07-02 14:48:29 +0900

    complete: react-todo-app
    react, typescriptでtodoアプリ作成

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,18 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 interface Todo {
   value: string
+  id: number
+  checked: boolean
+  removed: boolean
 }
 
+type Filter = 'all' | 'checked' | 'unchecked' | 'removed'
+
 const App: React.VFC = () => {
+  const [text, setText] = useState('')
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [filter, setFilter] = useState<Filter>('all')
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement | HTMLInputElement>) => {
+    e.preventDefault()
+
+    if(!text) return
+
+    const newTodo: Todo = {
+      value: text,
+      id: new Date().getTime(),
+      checked: false,
+      removed: false
+    }
+
+    setTodos([newTodo, ...todos])
+    setText('')
+  }
+
+  const handleOnEdit = (id: number, value: string) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.value = value
+      }
+      return todo
+    })
+    setTodos(newTodos)
+  }
+
+  const handleOnCheck = (id: number, checked: boolean) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.checked = !checked;
+      }
+      return todo
+    })
+    setTodos(newTodos)
+  }
+
+  const handleOnRemove = (id: number, removed: boolean) => {
+    const newTodos = todos.map((todo) => {
+      if(todo.id === id) {
+        todo.removed = !removed
+      }
+      return todo
+    })
+    setTodos(newTodos)
+  }
+
+  const handleOnEmpty = () => {
+    const newTodos = todos.filter((todo) => !todo.removed)
+    setTodos(newTodos)
+  }
+
+  const filteredTodos = todos.filter((todo) => {
+    switch (filter) {
+      case 'all':
+        return !todo.removed
+      case 'checked':
+        return todo.checked && !todo.removed
+      case 'unchecked':
+        return !todo.checked && !todo.removed
+      case 'removed':
+        return todo.removed
+      default:
+        return todo
+    }
+  })
+
   return (
     <div className="App">
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input type='text' value={''} onChange={(e) => e.preventDefault()} />
-        <input type='submit' value={'追加'} onChange={(e) => e.preventDefault()} />
-      </form>
+      <select defaultValue='all' onChange={(e) => setFilter(e.target.value as Filter)}>
+        <option value='all'>すべてのタスク</option>
+        <option value='checked'>完了したタスク</option>
+        <option value='unchecked'>未完了のタスク</option>
+        <option value='removed'>削除済みのタスク</option>
+      </select>
+      {filter === 'removed' ? (
+        <button onClick={() => handleOnEmpty()}>ゴミ箱を空にする</button>
+      ) : (
+        <form onSubmit={(e) => handleOnSubmit(e)}>
+          <input type='text' value={text} disabled={filter === 'checked'} onChange={(e) => setText(e.target.value)} />
+          <input type='submit' value={'追加'} disabled={filter === 'checked'} onSubmit={(e) => handleOnSubmit(e)} />
+        </form>
+      )}
+      <ul>
+        {filteredTodos.map(todo => {
+          return (
+            <li key={todo.id}>
+              <input type='checkbox' disabled={todo.removed} checked={todo.checked} onChange={() => handleOnCheck(todo.id, todo.checked)}/>
+              <input type='text' disabled={todo.checked || todo.removed} value={todo.value} onChange={(e) => handleOnEdit(todo.id, e.target.value)} />
+              <button onClick={() => handleOnRemove(todo.id, todo.removed)}>{todo.removed ? '復元' : '削除'}</button>
+            </li>
+          )
+        })}
+      </ul>
+      {console.log(todos)}
     </div>
   );
 }
